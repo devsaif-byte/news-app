14b64ef30f734bfa9f1c892c304705f8c16de5e2
devsaif-byte <saifuddinrabib654@gmail.com>
2023-03-09 01:36:23 +0600

    Add build folder

---
+++ build/precache-manifest.fa794d774af5defebd94c7acf05760b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bc790d68b3753c105c0",
+    "url": "/static/css/main.39144455.chunk.css"
+  },
+  {
+    "revision": "9bc790d68b3753c105c0",
+    "url": "/static/js/main.df31a74a.chunk.js"
+  },
+  {
+    "revision": "920859be891436379fe2",
+    "url": "/static/js/runtime~main.c2259ff6.js"
+  },
+  {
+    "revision": "9860ddfa7377b3971205",
+    "url": "/static/js/2.e6d328d8.chunk.js"
+  },
+  {
+    "revision": "4e17d60eb3c8bbf63848",
+    "url": "/static/js/3.fa6a646c.chunk.js"
+  },
+  {
+    "revision": "a2b92e3ad48923f77c96dd35fbaac032",
+    "url": "/index.html"
+  }
+];
